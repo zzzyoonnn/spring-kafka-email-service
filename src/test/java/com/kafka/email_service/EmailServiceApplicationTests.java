87d729443b1858cef9e0bc880d494f951fa7e6b4
zzzyoonnn <dwsrh03@gmail.com>
2026-06-27 15:49:57 +0900

    chore: initialize email-service project

---
+++ src/test/java/com/kafka/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
